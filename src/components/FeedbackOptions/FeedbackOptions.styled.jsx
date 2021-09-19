2984bf5a79d4f731809a817696e67b486b8b5e88
Tiutiunnyk Elena <elena.tiutiunnyk@gmail.com>
2021-09-19 15:50:15 +0300

    feedback hw-2

---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const ButtonFeedback = styled.button`
+  border-radius: 4px;
+  border: solid grey;
+  border-radius: 4px;
+  border: solid grey;
+  margin-left: 24px;
+`;
